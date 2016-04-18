18d6a3639fae4a6dacfe25f9fe1f4f354fafcb34
Jin <gkdldi5@naver.com>
2016-04-19 06:10:49 +0900

    test

--- MiniProject/src/com/jin/controller/Controller.java
+++ MiniProject/src/com/jin/controller/Controller.java
@@ -12,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Controller
  */
 @WebServlet(
-		urlPatterns = { "/Controller" }, 
+		urlPatterns = { "/Controller,"
+				+ "*.board,"
+				+ "*.user" }, 
 		initParams = { 
 				@WebInitParam(name = "propertyConfig", value = "command.properties")
 		})
@@ -30,16 +32,14 @@ public class Controller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
 	}
 
 }
