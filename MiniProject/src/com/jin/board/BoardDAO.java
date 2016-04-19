4b41735915104c912b35f5e560a89177325d42a2
ksh <kosta@192.168.0.118>
2016-04-19 10:11:21 +0900

    4-19  10시10분

--- MiniProject/src/com/jin/board/BoardDAO.java
+++ MiniProject/src/com/jin/board/BoardDAO.java
@@ -13,31 +13,26 @@ public class BoardDAO implements BoardDAOInter {
 
 	@Override
 	public Connection getConnection() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<BoardDTO> getBoard() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void insertArticle(BoardDTO board) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public int updateBoard(BoardDTO board) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int deleteBoard(int articleNum, String pw) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
