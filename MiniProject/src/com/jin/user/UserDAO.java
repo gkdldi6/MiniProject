f9f44f025573708e804e21e8631f671117e897d5
ksh <kosta@192.168.0.118>
2016-04-19 10:18:22 +0900

    자동화함

--- MiniProject/src/com/jin/user/UserDAO.java
+++ MiniProject/src/com/jin/user/UserDAO.java
@@ -6,49 +6,41 @@ public class UserDAO implements UserDAOInter {
 
 	@Override
 	public UserDTO getInstance() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Connection getConnection() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void insertUser(UserDTO user) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public int userCheck(String id, String pw) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int confirmId(String id) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public UserDTO getUser(String id, String pw) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int updateUser(UserDTO user) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public int deleteUser(String id, String pw) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 	
